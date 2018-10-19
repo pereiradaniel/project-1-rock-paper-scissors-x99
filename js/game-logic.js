ca180e53005a9d9440de311556dffd91c37bb6fa
Daniel Pereira <danielpaulpereira@protonmail.com>
2018-10-19 17:27:35 -0400

    Breaking at invalid move type

--- js/game-logic.js
+++ js/game-logic.js
@@ -12,35 +12,56 @@ let playerTwoMoveOneValue;
 let playerTwoMoveTwoValue;
 let playerTwoMoveThreeValue;
 
-const setPlayerMoves = (player, m1, v1, m2, v2, m3, v3) => {
-  if (player === 'Player One') {
-    if (typeof m1 === 'string' && typeof m2 === 'string' && typeof m3 === 'string') {
-      playerOneMoveOneType = m1;
-      playerOneMoveTwoType = m2;
-      playerOneMoveThreeType = m3;
-    } else {};
+const ROCK = 'rock';
+const PAPER = 'paper';
+const SCISSORS = 'scissors';
 
-    if (typeof v1 === 'number' && typeof v2 === 'number' && typeof v3 === 'number') {
+const setPlayerMoves = (player, m1, v1, m2, v2, m3, v3) => {
+  // Check that all values are provided
+  if (!m1 || !m2 || !m3 || !v1 || !v2 || !v3) {
+    return;
+  };
+
+  // Checks that values are valid types
+  if (!validTypes(m1, m2, m3)) {
+    return;
+  };
+
+  if (!validValues(v1, v2, v3)) {
+    return;
+  };
+
+  switch (player) {
+    case 'Player One':
+    playerOneMoveOneType = m1;
+    playerOneMoveTwoType = m2;
+    playerOneMoveThreeType = m3;
     playerOneMoveOneValue = v1;
     playerOneMoveTwoValue = v2;
     playerOneMoveThreeValue = v3;
-    } else {};
+    break;
 
-  } else if (player === 'Player Two') {
-    if (typeof m1 === 'string' && typeof m2 === 'string' && typeof m3 === 'string') {
-      playerTwoMoveOneType = m1;
-      playerTwoMoveTwoType = m2;
-      playerTwoMoveThreeType = m3;
-    } else {};
-
-    if (typeof v1 === 'number' && typeof v2 === 'number' && typeof v3 === 'number') {
+  case 'Player Two':
+    playerTwoMoveOneType = m1;
+    playerTwoMoveTwoType = m2;
+    playerTwoMoveThreeType = m3;
     playerTwoMoveOneValue = v1;
     playerTwoMoveTwoValue = v2;
     playerTwoMoveThreeValue = v3;
-    } else {};
-  };
+    break;
+    
+  }
+
 };
 
 const getRoundWinner = () => {
 
 };
+
+const validTypes = (move1, move2, move3) =>
+  validType(move1) && validType(move2) && validType(move3);
+
+const validType = (type) => type === ROCK || type === PAPER || SCISSORS;
+
+const validValues = (value1, value2, value3) =>
+  value1 >= 1 && value2 >= 1 && value3 >= 1 && value1 + value2 + value3 < 100;
