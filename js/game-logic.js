43820564f1cf3e0b7dcbd45d96e4446d80000266
Daniel Pereira <danielpaulpereira@protonmail.com>
2018-10-19 17:04:04 -0400

    Passes some tests

--- js/game-logic.js
+++ js/game-logic.js
@@ -1,1 +1,46 @@
-// All code should be written in this file.
+let playerOneMoveOneType;
+let playerOneMoveTwoType;
+let playerOneMoveThreeType;
+let playerTwoMoveOneType;
+let playerTwoMoveTwoType;
+let playerTwoMoveThreeType;
+
+let playerOneMoveOneValue;
+let playerOneMoveTwoValue;
+let playerOneMoveThreeValue;
+let playerTwoMoveOneValue;
+let playerTwoMoveTwoValue;
+let playerTwoMoveThreeValue;
+
+const setPlayerMoves = (player, m1, v1, m2, v2, m3, v3) => {
+  if (player === 'Player One') {
+    if (typeof m1 === 'string' && typeof m2 === 'string' && typeof m3 === 'string') {
+      playerOneMoveOneType = m1;
+      playerOneMoveTwoType = m2;
+      playerOneMoveThreeType = m3;
+    } else {};
+
+    if (typeof v1 === 'number' && typeof v2 === 'number' && typeof v3 === 'number') {
+    playerOneMoveOneValue = v1;
+    playerOneMoveTwoValue = v2;
+    playerOneMoveThreeValue = v3;
+    } else {};
+
+  } else if (player === 'Player Two') {
+    if (typeof m1 === 'string' && typeof m2 === 'string' && typeof m3 === 'string') {
+      playerTwoMoveOneType = m1;
+      playerTwoMoveTwoType = m2;
+      playerTwoMoveThreeType = m3;
+    } else {};
+
+    if (typeof v1 === 'number' && typeof v2 === 'number' && typeof v3 === 'number') {
+    playerTwoMoveOneValue = v1;
+    playerTwoMoveTwoValue = v2;
+    playerTwoMoveThreeValue = v3;
+    } else {};
+  };
+};
+
+const getRoundWinner = () => {
+
+};
