e6ac73e66df94afabcddb334cde671c437c217ab
Daniel Pereira <danielpaulpereira@protonmail.com>
2018-10-20 20:11:41 -0400

    Passes all tests except for bonus.

--- js/game-logic.js
+++ js/game-logic.js
@@ -17,6 +17,7 @@ const SCISSORS = 'scissors';
 
 const P1 = 'Player One';
 const P2 = 'Player Two';
+const TIE = 'Tie';
 
 const setPlayerMoves = (player, m1, v1, m2, v2, m3, v3) => {
   // Check that all values are provided
@@ -51,19 +52,108 @@ const setPlayerMoves = (player, m1, v1, m2, v2, m3, v3) => {
     playerTwoMoveTwoValue = v2;
     playerTwoMoveThreeValue = v3;
     break;
-    
   }
-
 };
+
 
 const validTypes = (move1, move2, move3) =>
   validType(move1) && validType(move2) && validType(move3);
 
+
 const validType = (type) => type === ROCK || type === PAPER || type === SCISSORS;
+
 
 const validValues = (value1, value2, value3) =>
   value1 >= 1 && value2 >= 1 && value3 >= 1 && value1 + value2 + value3 < 100;
 
-const getRoundWinner = () => {
 
+const getRoundWinner = (round) => {
+  switch (round) {
+    case 1:
+      return getMoveWinner(playerOneMoveOneType, playerOneMoveOneValue,
+                    playerTwoMoveOneType, playerTwoMoveOneValue);
+      break;
+    case 2:
+      return getMoveWinner(playerOneMoveTwoType, playerOneMoveTwoValue,
+                    playerTwoMoveTwoType, playerTwoMoveTwoValue);
+      break;
+    case 3:
+      return getMoveWinner(playerOneMoveThreeType, playerOneMoveThreeValue,
+                    playerTwoMoveThreeType, playerTwoMoveThreeValue);
+      break
+    default:
+      return null;
+  };
+};
+
+
+const getMoveWinner = (p1moveType, p1moveValue, p2moveType, p2moveValue) => {
+  if (!p1moveType || !p1moveValue || !p2moveType || !p2moveValue) {
+    return null;
+  };
+
+  if (p1moveType === p2moveType) {
+    if (p1moveValue > p2moveValue) {
+      return P1;
+    } else if (p1moveValue < p2moveValue) {
+      return P2;
+    } else {
+      return TIE;
+    }
+  } else if (p1moveType === ROCK) {
+    if (p2moveType === SCISSORS) {
+      return P1;
+    } else if (p2moveType === PAPER) {
+      return P2;
+    }
+  } else if (p1moveType === PAPER) {
+    if (p2moveType === ROCK) {
+      return P1;
+    } else if (p2moveType === SCISSORS) {
+      return P2;
+    }
+  } else if (p1moveType === SCISSORS) {
+    if (p2moveType === ROCK) {
+      return P2;
+    } else if (p2moveType === PAPER) {
+      return P1;
+    }
+  }
+};
+
+const getGameWinner = () => {
+  if (!playerOneMoveOneType || !playerOneMoveTwoType || !playerOneMoveThreeType || 
+      !playerTwoMoveOneType || !playerTwoMoveTwoType || !playerTwoMoveThreeType ||
+      !playerOneMoveOneValue || !playerOneMoveTwoValue || !playerOneMoveThreeValue ||
+      !playerTwoMoveOneValue || !playerTwoMoveTwoValue || !playerTwoMoveThreeValue) {
+    return null;
+  };
+
+  playerOneWins = 0;
+  playerTwoWins = 0;
+
+  const winnerRoundOne = getRoundWinner(1);
+  const winnerRoundTwo = getRoundWinner(2);
+  const winnerRoundThree = getRoundWinner(3);
+
+
+  addWin(winnerRoundOne);
+  addWin(winnerRoundTwo);
+  addWin(winnerRoundThree);
+
+  if (playerOneWins > playerTwoWins) {
+    return 'Player One';
+  } else if (playerTwoWins > playerOneWins) {
+    return 'Player Two';
+  } else {
+    return 'Tie';
+  }
+};
+
+const addWin = (winner) => {
+  if (winner === 'Player One') {
+    playerOneWins = (playerOneWins + 1) || 1;
+  } else if (winner === 'Player Two') {
+    playerTwoWins = (playerTwoWins + 1) || 1;
+  }
 };
